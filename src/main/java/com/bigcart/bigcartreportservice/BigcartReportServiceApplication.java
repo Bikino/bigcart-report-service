eb804c19b66f2f9afdb7a4f46f40ec809ad6bc14
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-10-21 12:37:58 -0400

    First commmit

---
+++ src/main/java/com/bigcart/bigcartreportservice/BigcartReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigcart.bigcartreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcartReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigcartReportServiceApplication.class, args);
+    }
+
+}
